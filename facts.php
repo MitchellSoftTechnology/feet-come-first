644d18676f3e9f277d68dc27a1bababbad560556
Kate Mitchell <mail@katemitchell.org.uk>
2018-11-28 16:42:32 +0000

    Fix encoding errors

--- facts.php
+++ facts.php
@@ -13,19 +13,19 @@ include 'includes/navigation.php';
 	<ul id="facts">
 		<li>Leonardo Da Vinci called the human foot <em>"The greatest engineering device in the whole world"</em>
 		and it is hard to argue with that. The average person walks about 10,000 steps each day, with each
-		step having to bear around 2½ times our body weight. Across a lifetime this equates to travelling a
+		step having to bear around 2&#189; times our body weight. Across a lifetime this equates to travelling a
 		distance of around 115,000 miles, or more than 4 times around the world. Small wonder then that our
 		feet can give us problems without us giving them the necessary care and attention they deserve.
 		Further on, I provide some useful suggestions for helping to minimise the chances of problems with
 		our feet.</li>
 
-		<li>The world’s largest known feet belong to Matthew McGrory, a 7'4" resident of Florida whose size 27½
+		<li>The world's largest known feet belong to Matthew McGrory, a 7'4" resident of Florida whose size 27&#189;
 		(UK size) feet require specially-made footwear costing around £14,000.</li>
 
 		<li>Our feet are made up of 52 bones, representing 25% of all our bones.</li>
 
 		<li>Women suffer with around 4 times as many foot problems than men. This is largely due to women's
-		choice of footwear – often narrow toes and higher heels. This is compounded by them walking an average
+		choice of footwear - often narrow toes and higher heels. This is compounded by them walking an average
 		of 3 miles further than men each day, coupled with their average height differential requiring more
 		steps to cover the same distance.</li>
 
